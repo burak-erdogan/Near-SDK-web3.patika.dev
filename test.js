9145370d37817d953ce57c239a0c8f59306f30c1
burak-erdogan <86931894+burak-erdogan@users.noreply.github.com>
2022-04-05 14:39:05 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,28 @@
+import { connect, Contract, keyStores, WalletConnection } from 'near-api-js'
+import getConfig from './config'
+
+const nearConfig = getConfig(process.env.NODE_ENV || 'development')
+
+// Initialize contract & set global variables
+export async function initContract() {
+// Initialize connection to the NEAR testnet.
+// Note that the keys are saved in local storage and never leave the client!
+const near = await connect(
+Object.assign({ deps: { keyStore: new keyStores.BrowserLocalStorageKeyStore() } }, nearConfig),
+)
+
+// Initializing Wallet based Account. It can work with NEAR testnet wallet that
+// is hosted at https://wallet.testnet.near.org
+window.walletConnection = new WalletConnection(near)
+
+// Getting the Account ID. If still unauthorized, it's just empty string
+window.accountId = window.walletConnection.getAccountId()
+
+// Initializing our contract APIs by contract name and configuration
+window.contract = await new Contract(window.walletConnection.account(), nearConfig.contractName, {
+// View methods are read only. They don't modify the state, but usually return some value.
+viewMethods: ['get_meme_list'],
+// Change (“call”) methods can modify the state. But you don't receive the returned value when called.
+changeMethods: ['create_meme'],
+})
+}
